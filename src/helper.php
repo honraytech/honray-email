4671bc6c5799264907f5ae8d633e89b810c285f7
honraytech <tech@honraytech.com>
2017-01-05 13:40:52 +0800

    test
    test

--- src/helper.php
+++ src/helper.php
@@ -31,38 +31,38 @@
 use think\email\PHPMailer;
 function send_email($config, $to, $name, $subject, $body, $attachment = null, $src = null)
 {
-        $mail = new PHPMailer();                                // PHPMailer对象
-        $mail->CharSet = 'UTF-8';                               // 设置邮件编码
-        $mail->IsSMTP();                                        // 设定使用SMTP服务
-        $mail->IsHTML(true);                                    // 可以发送html格式
-        $mail->AddEmbeddedImage($src);                          // 设置邮件中的图片      
-        $mail->SMTPDebug = 0;                                   // 关闭SMTP调试功能
-        $mail->SMTPAuth = true;                                 // 启用 SMTP 验证功能
-        if ($config['smtp_port'] == 465){
-            $mail->SMTPSecure = 'ssl';                          // 使用安全协议
-        }
-        $mail->Host = $config['smtp_host'];                     // SMTP 服务器
-        $mail->Port = $config['smtp_port'];                     // SMTP服务器的端口号
-        $mail->Username = $config['smtp_user'];                 // SMTP服务器用户名
-        $mail->Password = $config['smtp_pass'];                 // SMTP服务器密码
-        $mail->SetFrom($config['from_email'], $config['from_name']);
-        $replyEmail = $config['reply_email'] ? $config['reply_email'] : $config['reply_email'];
-        $replyName = $config['reply_name'] ? $config['reply_name'] : $config['reply_name'];
-        $mail->AddReplyTo($replyEmail, $replyName);
-        $mail->Subject = $subject;
-        $mail->MsgHTML($body);
-        $mail->AddAddress($to, $name);
-        if (is_array($attachment)) { // 添加附件
-            foreach ($attachment as $file) {
-                if (is_array($file)) {
-                    is_file($file['path']) && $mail->AddAttachment($file['path'], $file['name']);
-                } else {
-                    is_file($file) && $mail->AddAttachment($file);
-                }
+    $mail = new PHPMailer();                                // PHPMailer对象
+    $mail->CharSet = 'UTF-8';                               // 设置邮件编码
+    $mail->IsSMTP();                                        // 设定使用SMTP服务
+    $mail->IsHTML(true);                                    // 可以发送html格式
+    $mail->AddEmbeddedImage($src);                          // 设置邮件中的图片      
+    $mail->SMTPDebug = 0;                                   // 关闭SMTP调试功能
+    $mail->SMTPAuth = true;                                 // 启用 SMTP 验证功能
+    if ($config['smtp_port'] == 465){
+        $mail->SMTPSecure = 'ssl';                          // 使用安全协议
+    }
+    $mail->Host = $config['smtp_host'];                     // SMTP 服务器
+    $mail->Port = $config['smtp_port'];                     // SMTP服务器的端口号
+    $mail->Username = $config['smtp_user'];                 // SMTP服务器用户名
+    $mail->Password = $config['smtp_pass'];                 // SMTP服务器密码
+    $mail->SetFrom($config['from_email'], $config['from_name']);
+    $replyEmail = $config['reply_email'] ? $config['reply_email'] : $config['reply_email'];
+    $replyName = $config['reply_name'] ? $config['reply_name'] : $config['reply_name'];
+    $mail->AddReplyTo($replyEmail, $replyName);
+    $mail->Subject = $subject;
+    $mail->MsgHTML($body);
+    $mail->AddAddress($to, $name);
+    if (is_array($attachment)) { // 添加附件
+        foreach ($attachment as $file) {
+            if (is_array($file)) {
+                is_file($file['path']) && $mail->AddAttachment($file['path'], $file['name']);
+            } else {
+                is_file($file) && $mail->AddAttachment($file);
             }
-        } else {
-            is_file($attachment) && $mail->AddAttachment($attachment);
         }
-        return $mail->Send() ? true : $mail->ErrorInfo;
+    } else {
+        is_file($attachment) && $mail->AddAttachment($attachment);
+    }
+    return $mail->Send() ? true : $mail->ErrorInfo;
 }
 ?>
